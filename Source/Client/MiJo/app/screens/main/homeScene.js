420d2b1468560545422908d58ab5395424215fc6
Tobias Madner <tobias.madner@gmx.at>
2016-04-21 11:54:34 +0200

    Revert "Solved Error with case-sensitivity with HomeScene.js"
    This reverts commit d8420baaa7e8236e4b0174efa1be08142bdb3ad4.

--- Source/Client/MiJo/app/screens/main/homeScene.js
+++ Source/Client/MiJo/app/screens/main/homeScene.js
@@ -3,14 +3,11 @@
 // imports
 import React from 'react-native'
 import GlobalStyle from './GlobalStyle'
-import ControlPanel from './ControlPanel'
 import Card from './Card'
-import MessageScene from './MessageScene'
 import Api from 'MiJo/app/Api'
-import Drawer from 'react-native-drawer'
-import NavBarNavigationDrawer from 'MiJo/app/components/navbar/NavBarNavigationDrawer'
 
-
+// temp
+import LoginButton from 'MiJo/app/components/buttons/LoginButton'
 
 // global vars
 var {
@@ -30,35 +27,23 @@ class HomeScene extends React.Component {
   constructor(props) {
     super(props);
   }
-  closeDrawer = () => {
-    this._drawer.close()
-  };
-  openDrawer = () => {
-    this._drawer.open()
-  };
 
   render() {
     return (
-      <Drawer
-        type="overlay"
-        ref={(ref) => this._drawer = ref}
-        content={<ControlPanel navigator={this.props.navigator}/>}
-        tapToClose={true}
-        openDrawerOffset={0.2}
-        panCloseMask={0.2}
-        closedDrawerOffset={-3}
-        styles={{
-          drawer: {shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
-          main: {paddingLeft: 3}
-        }}
-        tweenHandler={(ratio) => ({
-          main: { opacity:(2-ratio)/2 }
-        })}
-        >
-        <NavBarNavigationDrawer title="MiJo" onPress={this.openDrawer}/>
-        <Card/>
-      </Drawer>
+      <View style={GlobalStyle.container}>
+        <LoginButton onPress={() => this._handleCreateOffer()} text="Create Offer"/>
+        <Card style={{flex: 1}}/>
+      </View>
     );
+
+
+  }
+
+  _handleCreateOffer() {
+    console.log('Create Offer pressed');
+    this.props.navigator.push({
+      id: 'CreateOffer'
+    });
   }
 }
 
